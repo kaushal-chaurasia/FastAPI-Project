677b5da123b7a48b123e7016510976b9594814b6
Kaushal Chaurasia <kaushalchaurasia2004@gmail.com>
2025-08-19 05:24:51 +0530

    Initial commit

---
+++ blog/main2.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from .database import engine
+from . import models
+from .routers import blog , user,authentication
+
+app = FastAPI()
+
+# create all tables
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
+
+
+
+
--- mainone.py
+++ mainone.py
@@ -1,15 +1,57 @@
 #import 
 from fastapi import FastAPI
+from typing import Optional  
+from pydantic import BaseModel
+from uvicorn import run as uvicorn
 
 #instance
 app = FastAPI()
 
-#decorator
-@app.get("/")
-#function
-def index():
-    return {'data':{'name':'Sarthak'}}
 
-@app.get("/about")
-def about():
-    return {'data':{'about page'}}
+
+
+#decorator
+@app.get("/blog")
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        # only get 10 published blogs
+        return {'data': f'{limit} published blogs from the database'}
+    else:
+        return {'data': f'{limit} blogs from the database'}
+
+
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    #fetch unpublished blogs
+    return {'data':'all unpublished blogs'}
+
+
+
+
+@app.get('/blog/{id}')
+def show(id:int):
+    #fetch blog with id=id
+    return {'data':id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id:int,limit=10):
+    #fetch comments for blog with id=id
+    return {'data':['1','2']}
+
+class Blog(BaseModel):
+   title:str
+   body:str
+   published:Optional[bool]
+   
+   
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    #create a new blog
+    return {'data': f'Blog created with title as{blog.title}'}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
