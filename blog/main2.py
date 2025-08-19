19097e27b9220b15b1189dc542acf4e1e927646a
Kaushal Chaurasia <kaushalchaurasia2004@gmail.com>
2025-08-19 07:20:07 +0530

    Fix: remove uvicorn.run for Railway

--- blog/main2.py
+++ blog/main2.py
@@ -19,5 +19,4 @@ app.include_router(user.router)
 def home():
     return {"message": "Hello from Railway!"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
